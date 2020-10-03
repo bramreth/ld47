4c9f5bc0141aa399b65e25f190f9f441fc393b6f
bram <bramreth@gmail.com>
2020-10-03 10:57:30 +0100

    weak spots

--- loop_blaster/Line2D.gd
+++ loop_blaster/Line2D.gd
@@ -5,6 +5,7 @@ var origin
 var time = 0
 var roc = 8
 var pulse_mag = 35
+var weak_seg = {}
 export(int) var distance = 75
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -13,9 +14,18 @@ func _process(delta):
 		var pulse_rate = (distance/get_parent().max_width)
 		var off = n.get_noise_2d(points[point].x+ time, points[point].y + time)*(pulse_rate)*pulse_mag
 		points[point] = origin[point] * distance + Vector2(off, off)
+		if point in weak_seg.keys():
+			$WeakSpot.points[weak_seg[point]] = origin[point] * distance + Vector2(off, off)
 		
 		
 		
-func init():
+func init(var weak_segs):
 	distance = get_parent().max_width
 	origin = points
+	randomize()
+	var start = randi()%get_point_count()
+	for i in range(weak_segs):
+		var p = (start + i) % get_point_count()
+		print(p)
+		weak_seg[p] = i
+		$WeakSpot.add_point(points[p])
--- loop_blaster/loop.gd
+++ loop_blaster/loop.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 export(int) var max_width = 75
 export(int) var segments = 16
+export(int) var weak_segments = 4
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,13 +11,11 @@ func _ready():
 	create_loop()
 
 func create_loop():
-	print(range(segments))
 	$Line2D.add_point(Vector2(cos(0), sin(0)))
 	for seg in range(segments):
 		var p = deg2rad((float(seg+1) / segments) * 360.0)
 		$Line2D.add_point(Vector2(cos(p), sin(p)))
-		print(p)
-	$Line2D.init()
+	$Line2D.init(weak_segments)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
@@ -28,3 +27,7 @@ func _on_Button_pressed():
 
 func _on_CurveTween_curve_tween(sat):
 	$Line2D.distance = max_width * sat
+
+
+func _on_CurveTween_tween_all_completed():
+	queue_free()
