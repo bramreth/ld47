1997c0b33a4c6755d4f6c890de6bfd90fcfcac37
Maximilian Taubert <m.taubert2015@gmail.com>
2020-10-03 10:12:52 +0100

    added player controller

---
+++ loop_blaster/UI.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+
+
+func change_input_method():
+	match(inputmanager.control_preference):
+		inputmanager.CONTROL_TYPE.KEYBOARD: inputmanager.change_control_type(inputmanager.CONTROL_TYPE.MOUSE)
+		inputmanager.CONTROL_TYPE.MOUSE: inputmanager.change_control_type(inputmanager.CONTROL_TYPE.KEYBOARD)
---
+++ loop_blaster/inputmanager.gd
@@ -0,0 +1,15 @@
+extends Node
+
+signal update_control_preference(control_preference)
+
+enum CONTROL_TYPE {
+	MOUSE,
+	KEYBOARD
+}
+
+var control_preference:int = CONTROL_TYPE.KEYBOARD
+
+
+func change_control_type(new_preference:int):
+	control_preference = new_preference
+	emit_signal("update_control_preference", control_preference)
